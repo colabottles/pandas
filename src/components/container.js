d6341ef026bccf0c1ff62ce81393c43df1118a12
Todd Libby <toddlibby@protonmail.com>
2019-07-08 15:13:01 -0400

    Trying to get class sttyling to work

--- src/components/container.js
+++ src/components/container.js
@@ -2,10 +2,16 @@ import React from "react"
 import styled from "styled-components"
 
 const Wrapper = styled.wrapper`
+  background-color: rebeccapurple;
   margin: 3rem auto;
   max-width: 600px;
 `
 
-export default ({ children }) => (
-  <Wrapper className={`wrapper`}>{children}</Section>
-)
+const Layout = () => {
+  return (
+    // add a stable CSS class
+    <Wrapper className={`wrapper`}>
+      
+    </Wrapper>
+  )
+}
