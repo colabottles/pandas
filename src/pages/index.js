0ba1521d82555eb1b10487e1bfcf953f1d744c9b
Todd Libby <toddlibby@protonmail.com>
2020-08-02 23:49:38 -0400

    Updated Gatsby-CLI.

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,29 +48,23 @@ export default ({ data }) => {
         )
       }
       
-      export const query = graphql`
-        query {
-          allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
-            edges {
-              node {
-                id
-                frontmatter {
-                  title
-                  date(formatString: "MMMM DD, YYYY")
-                  path
-                  tags
-                  excerpt
-                }
-                timeToRead
-                fields {
-                  slug
-                  readingTime {
-                    text
-                  }
-                }
-                excerpt(pruneLength: 200)
+    export const query = graphql`
+      query {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+          totalCount
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date(formatString: "DD MMMM, YYYY")
               }
+              fields {
+                slug
+              }
+              excerpt
             }
           }
         }
-      `
+      }
+    `
